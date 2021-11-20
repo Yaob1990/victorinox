3e68eb8331dd9c49d976433a6998daa2e8eac40a
Yaob1990 <yekongbuye@gmail.com>
2021-11-20 15:02:52 +0800

    refactor: sleep 导出调整

--- src/timer/index.ts
+++ src/timer/index.ts
@@ -1,5 +1,3 @@
-async function sleep(ms: number): Promise<void> {
+export function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
-
-export { sleep }
