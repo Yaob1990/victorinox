9186042e314fbc8c5ef4762f502305fe2811b0bc
Yaob1990 <>
2021-11-20 07:05:50 +0000

    Deploying to gh-pages from @ Yaob1990/victorinox@515324c9b6f851b389bfb47cb325f9e3511a29ae 🚀

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './promiseThrottle'
-export * from './timer'
-export * from './emoji'
--- tools/cat.ts
+++
@@ -1,9 +0,0 @@
-import { readFile } from 'fs'
-
-readFile(process.argv[2], function (err, data) {
-  if (err != null) {
-    console.log(err)
-    return
-  }
-  process.stdout.write(data)
-})
